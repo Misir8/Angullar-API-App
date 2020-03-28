0fa94abaafe01fc87674b2c42ae811427a22388b
Misir <misiraskerov1@gmail.com>
2020-03-28 14:11:31 +0400

    Initial Create

---
+++ AngularWithASP/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using AngularWithASP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularWithASP.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        
+        public DbSet<WeatherForecast> WeatherForecasts { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ AngularWithASP/Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace AngularWithASP.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
